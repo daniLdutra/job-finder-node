3ab5fb686a2a7b55c8c4fb8b1ce22113430f5a37
Daniella Dutra <danivetdutra@gmail.com>
2022-03-01 17:05:30 -0300

    Criada view para ver detalhes da vaga e interacao com backend

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,10 +8,23 @@ router.get('/test', (req, res) => {
   res.send('deu certo!!!');
 });
 
+//detalhe da vaga
+router.get('/view/:id', (req, res) =>
+  Job.findOne({ where: { id: req.params.id } })
+    .then((job) => {
+      res.render('view', {
+        job,
+      });
+    })
+    .catch((err) => console.log(err))
+);
+
+//form da rota de envio
 router.get('/add', (req, res) => {
   res.render('add');
 });
 
+//add job via post
 router.post('/add', (req, res) => {
   let { title, salary, company, description, email, new_job } = req.body;
 

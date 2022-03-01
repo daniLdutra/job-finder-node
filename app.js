d60e5e6699aaec72bfcb5775d60a31bc2572c4ec
Daniella Dutra <danivetdutra@gmail.com>
2022-03-01 16:22:50 -0300

    Criada logica para busca de vagas

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
 const Job = require('./models/Job');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 const PORT = 3500;
 
@@ -34,11 +36,30 @@ db.authenticate()
 
 //router
 app.get('/', (req, res) => {
-  Job.findAll({ order: [['createdAt', 'DESC']] }).then((jobs) => {
-    res.render('index', {
-      jobs,
-    });
-  });
+  let search = req.query.job;
+  let query = '%' + search + '%'; // PH = PHP; Word = Wordpress; press = Wordpress(auxilia na busca = versatilidade)
+
+  if (!search) {
+    Job.findAll({ order: [['createdAt', 'DESC']] })
+      .then((jobs) => {
+        res.render('index', {
+          jobs,
+        });
+      })
+      .catch((err) => console.log(err));
+  } else {
+    Job.findAll({
+      where: { title: { [Op.like]: query } },
+      order: [['createdAt', 'DESC']],
+    })
+      .then((jobs) => {
+        res.render('index', {
+          jobs,
+          search,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
 //jobs router
